d48343b743fa052cb31fa92713c508d43fe7886b
cuddleminddev <cuddleminddev@gmail.com>
2025-08-19 14:11:46 +0530

    footer

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-products',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
