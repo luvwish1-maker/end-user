5d9b683e09a305d830a8b95b69ad32e3ac665973
cuddleminddev <cuddleminddev@gmail.com>
2025-08-19 15:40:15 +0530

    products basic

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [HeaderComponent, FooterComponent, CommonModule],
+  imports: [HeaderComponent, FooterComponent, CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
   categories = [
     {
       id: 1,
@@ -46,6 +47,7 @@ export class ProductsComponent {
       price: '2499',
       originalPrice: '4999',
       stock: '25',
+      quantity: 1
     },
     {
       id: 2,
@@ -54,6 +56,7 @@ export class ProductsComponent {
       price: '2499',
       originalPrice: '4999',
       stock: '25',
+      quantity: 1
     },
     {
       id: 3,
@@ -62,6 +65,7 @@ export class ProductsComponent {
       price: '2499',
       originalPrice: '4999',
       stock: '25',
+      quantity: 1
     },
     {
       id: 4,
@@ -70,6 +74,7 @@ export class ProductsComponent {
       price: '2499',
       originalPrice: '4999',
       stock: '25',
+      quantity: 1
     },
     {
       id: 5,
@@ -78,6 +83,7 @@ export class ProductsComponent {
       price: '2499',
       originalPrice: '4999',
       stock: '25',
+      quantity: 1
     },
     {
       id: 6,
@@ -86,6 +92,21 @@ export class ProductsComponent {
       price: '2499',
       originalPrice: '4999',
       stock: '25',
+      quantity: 1
     },
   ]
+
+  ngOnInit() {
+  this.products.forEach(product => product.quantity = 1);
+}
+
+  increaseQty(product: any) {
+    if (!product.quantity) product.quantity = 1;
+    if (product.quantity < product.stock) product.quantity++;
+  }
+
+  decreaseQty(product: any) {
+    if (!product.quantity) product.quantity = 1;
+    if (product.quantity > 1) product.quantity--;
+  }
 }
